32c6bdc1cedd381e5f92aba70a637e1b3c20085e
jvifdz <javiason@gmail.com>
2020-12-22 11:30:05 +0100

    Salida de datos recogidos en la actividad Login

--- app/src/main/java/com/example/loginpmdm/ActividadFinal.java
+++ app/src/main/java/com/example/loginpmdm/ActividadFinal.java
@@ -1,5 +1,7 @@
 package com.example.loginpmdm;
 
+import android.content.Intent;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class ActividadFinal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_final);
+
+
+        Intent i = getIntent();
+        if (i!=null){
+            String nombre= i.getStringExtra("nombre");
+            String contraseña= i.getStringExtra("contraseña");
+            String correo= i.getStringExtra("correo");
+            if (nombre!=null){
+                Toast.makeText(this,"nombre: "+nombre+" contraseña: "+contraseña+" correo: "+correo,Toast.LENGTH_LONG).show();}
+        }
     }
 }
