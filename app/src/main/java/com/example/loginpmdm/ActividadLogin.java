60b259092f26ab1d26b6bccf4b374a35de8524dc
jvifdz-PORTATIL <javiason@gmail.com>
2020-12-22 10:34:48 +0100

    initial commit

---
+++ app/src/main/java/com/example/loginpmdm/ActividadLogin.java
@@ -0,0 +1,13 @@
+package com.example.loginpmdm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_login);
+    }
+}
---
+++ app/src/main/java/com/example/loginpmdm/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.loginpmdm;
+
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickEntrar(View view) {
+
+
+
+    }
+}
